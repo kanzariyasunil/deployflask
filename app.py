396e17289cdacb5f198bfa17148f0927baacf6ea
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2024-11-15 16:11:51 +0530

    using venv

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "this is my flask app"
+def index():
+    return render_template('index.html')
 
-@app.route('/yourname')
-def yourname():
-    return "Enter your name url"
 
-@app.route('/yourname/<id>')
-def enter(id):
-    return render_template('index.html',id = id)
+if __name__ == "__main__":
+    app.run(debug=True)
